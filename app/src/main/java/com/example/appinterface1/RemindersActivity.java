c158cbba06422568198d82f81b3f2589aee20124
Cole Bowyer <colesheldonbowyer@gmail.com>
2025-01-21 10:24:52 -0500

    Created buttons for the reminders page. Need to figure out how to make the buttons go to a new page.

--- app/src/main/java/com/example/appinterface1/RemindersActivity.java
+++ app/src/main/java/com/example/appinterface1/RemindersActivity.java
@@ -1,6 +1,8 @@
 package com.example.appinterface1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,15 @@ public class RemindersActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void showReminders(View v) {
+
+        Intent i = null;
+
+        i = new Intent(this, ShowRemindersAcitvity.class);
+
+        startActivity(i);
+
     }
 }
