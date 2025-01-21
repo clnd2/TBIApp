9795e0f3c25415cbe1ac0f944982d423ac66b676
wallacebd25 <wallacebd25@vmi.edu>
2025-01-21 09:47:40 -0500

    Fixed all the issues to have a basic screen and UI

---
+++ app/src/main/java/com/example/appinterface1/showScreen.java
@@ -0,0 +1,34 @@
+package com.example.appinterface1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//This import will be for the home button
+//import selectScreen
+
+public class showScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_show_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void homeScreen(View H) {
+        Intent homeScreen = new Intent(this, MainActivity.class);
+        startActivity(homeScreen);
+    }
+}
