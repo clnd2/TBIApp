9a7e1e12963d1d0e1495aa6d6f127eb15c16e152
wallacebd25 <wallacebd25@vmi.edu>
2025-01-23 08:57:50 -0500

    Fixed all the issues to have a basic screen and UI

--- app/src/main/java/com/example/appinterface1/pswdShow.java
+++ app/src/main/java/com/example/appinterface1/pswdShow.java
@@ -10,16 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-//This import will be for the home button
-//import selectScreen
-
-public class showScreen extends AppCompatActivity {
+public class pswdShow extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_show_screen);
+        setContentView(R.layout.activity_pswd_show);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
