161cb6a11389d020d94a6d622f09f597f5b9f327
fendercr25 <clnd2.va@gmail.com>
2025-01-23 10:48:09 -0500

    Date selection

--- app/src/main/java/com/example/appinterface1/CalendarActivity.java
+++ app/src/main/java/com/example/appinterface1/CalendarActivity.java
@@ -2,14 +2,19 @@ package com.example.appinterface1;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendar;
+    TextView date_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,24 @@ public class CalendarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        CalendarView Cal = findViewById(R.id.calendarView);
+        calendar = findViewById(R.id.calendarView);
+        date_view = findViewById(R.id.dateView);
 
+        // listener for when date selected is changed
+        calendar.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(
+                            @NonNull CalendarView view,
+                            int year,
+                            int month,
+                            int dayOfMonth)
+                    {
+                        String date = (month+1) + "-" + dayOfMonth + "-" + year;
+                        date_view.setText(date);
+                    }
+                }
+        );
 
     }
 }
