ace41707aceb6e590a5646ff333829f7da3cfd95
Caleb <78883521+clnd2@users.noreply.github.com>
2025-02-04 08:44:18 -0500

    calendar (#3)
    * Date selection
    
    * add back button to CalendarActivity
    
    * add back button to CalendarActivity using medina method
    
    * Fix warnings before push
    
    * Update Gradle to 8.7.3

--- app/src/main/java/com/example/appinterface1/CalendarActivity.java
+++ app/src/main/java/com/example/appinterface1/CalendarActivity.java
@@ -1,15 +1,23 @@
 package com.example.appinterface1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendar;
+    TextView date_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +29,33 @@ public class CalendarActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        CalendarView Cal = findViewById(R.id.calendarView);
+        calendar = findViewById(R.id.calendarView);
+        date_view = findViewById(R.id.dateView);
 
+        // listener for when date selected is changed
+        calendar.setOnDateChangeListener(
+                (view, year, month, dayOfMonth) -> {
+                    String date = (month+1) + "-" + dayOfMonth + "-" + year;
+                    date_view.setText(date);
+                }
+        );
+
+    }
+
+    public void changeActivity(View v) {
+        // when a button in main menu is pressed, will switch to corresponding activity
+
+        Intent i;
+        String title = ((Button) v).getText().toString();
+        switch (title) {
+            case "Back":
+                i = new Intent(this, MainActivity.class);
+                break;
+            default:
+                i = new Intent(this, CalendarActivity.class);
+
+        }
+        startActivity(i);
 
     }
 }
