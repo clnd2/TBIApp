711ea47a45cb050ba7af6bd0f5b2af1e028faf5c
liamn <liamnoonan2013@gmail.com>
2025-02-04 08:30:30 -0500

    Updated user profile and created a new activity for caretaker settings

--- app/src/main/java/com/example/appinterface1/CaretakerActivity.java
+++ app/src/main/java/com/example/appinterface1/CaretakerActivity.java
@@ -11,44 +11,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.example.appinterface1.ProfileActivity;
+import com.example.appinterface1.R;
+
+public class CaretakerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_caretaker);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void changeActivity(View p) {
+    public void changeActivity2(View c) {
         // when a button in main menu is pressed, will switch to corresponding activity
 
         Intent i = null;
-        String title = ((Button) p).getText().toString();
+        String title = ((Button) c).getText().toString();
         switch (title) {
-            case "Timezone":
-                //drop down menu for timezone
+            case "?":
+                //?
                 break;
-            case "Languages":
-                //drop down menu for languages
+            case "??":
+                //??
                 break;
-            case "Notifications":
-                //turning notifications on and off
+            case "???":
+                //???
                 break;
-            case "Caretaker Settings":
-                i = new Intent(this, CaretakerActivity.class);
+            case "Regular Settings":
+                i = new Intent(this, ProfileActivity.class);
                 break;
             default:
-                i = new Intent(this, ProfileActivity.class);
+                i = new Intent(this, CaretakerActivity.class);
 
         }
         startActivity(i);
 
     }
-}
 
+}
