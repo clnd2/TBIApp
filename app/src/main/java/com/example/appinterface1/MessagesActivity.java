97574f06cce64c1da543204ead59e05189ee39e9
fendercr25 <clnd2.va@gmail.com>
2025-02-11 10:26:03 -0500

    Updating messages activity

--- app/src/main/java/com/example/appinterface1/MessagesActivity.java
+++ app/src/main/java/com/example/appinterface1/MessagesActivity.java
@@ -1,6 +1,8 @@
 package com.example.appinterface1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,12 @@ public class MessagesActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void switchToHome(View v) {
+        // Switch to home screen
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+
     }
 }
