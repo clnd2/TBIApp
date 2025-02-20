b4ab65cd81d69f769134e9caac89ac1f43faa8cc
Cole Bowyer <colesheldonbowyer@gmail.com>
2025-02-20 09:40:11 -0500

    Still need to figure out the date and time. Might try to make a widget for user friendliness. This will make the ICAL easier to handle.

--- app/src/main/java/com/example/appinterface1/ShowRemindersActivity.java
+++ app/src/main/java/com/example/appinterface1/ShowRemindersActivity.java
@@ -3,12 +3,19 @@ package com.example.appinterface1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ShowRemindersActivity extends AppCompatActivity {
 
@@ -22,6 +29,9 @@ public class ShowRemindersActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        displayICS();
+
     }
 
     public void home(View v) {
@@ -33,4 +43,31 @@ public class ShowRemindersActivity extends AppCompatActivity {
         startActivity(i);
 
     }
+
+    private void displayICS ()
+    {
+        File directory = getFilesDir();
+        File icsFile = new File(directory, "event.ics");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        try
+        {
+            FileInputStream fis = new FileInputStream(icsFile);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            reader.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(stringBuilder.toString());
+    }
+
+
 }
--- app/src/main/java/com/example/appinterface1/setReminders.java
+++ app/src/main/java/com/example/appinterface1/setReminders.java
@@ -3,6 +3,7 @@ package com.example.appinterface1;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -13,8 +14,16 @@ import androidx.core.view.WindowInsetsCompat;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog;
 
-//static final String TAG = "MainActivity";
+//added
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.Calendar;
 
 public class setReminders extends AppCompatActivity {
 
@@ -37,7 +46,24 @@ public class setReminders extends AppCompatActivity {
         i = new Intent(this, ShowRemindersActivity.class);
 
         //save the text of each file and make it a show reminder
+        String icsContent = createICSFile();
+        saveICSFile(icsContent);
 
+        startActivity(i);
+
+    }
+
+    public void cancel(View v) {
+
+        Intent i = null;
+
+        i = new Intent(this, RemindersActivity.class);
+
+        startActivity(i);
+
+    }
+
+    private String createICSFile() {
         //get the text of the title
         EditText title = findViewById(R.id.uiTitle);
         String tit = title.getText().toString();
@@ -58,34 +84,61 @@ public class setReminders extends AppCompatActivity {
         EditText time = findViewById(R.id.uiTime);
         String tim = time.getText().toString();
 
-        //Log.d("TAG", tim); // displays the correct word
-
         //create an ICAL file
         StringBuilder icsContent = new StringBuilder();
         icsContent.append("BEGIN:VCALENDAR\n");
         icsContent.append("Version:2.0\n");
         icsContent.append("BEGIN:VEVENT\n");
         icsContent.append("SUMMARY:" + tit + "\n");
-        icsContent.append("DTSTART:\n");
-        icsContent.append("DTEND:\n");
+        icsContent.append("DTSTART;TZID=America/Denver:" + dat + "T" + tim +"\n");
+        icsContent.append("DTEND;TZID=America/Denver:\n");
         icsContent.append("LOCATION:" + loc + "\n");
         icsContent.append("DESCRIPTION:" + desc + "\n");
         icsContent.append("STATUS:CONFIRMED\n");
         icsContent.append("END:VEVENT\n");
         icsContent.append("END:VCALENDAR\n");
 
+        return icsContent.toString();
+    }
 
-        startActivity(i);
+    private void saveICSFile(String icsContent)
+    {
+        try {
+            // Get the path to the external storage directory
+            File directory = getFilesDir();
+
+            // Create a file for the ICS content
+            File icsFile = new File(directory, "event.ics");
+            FileOutputStream fileOutputStream = new FileOutputStream(icsFile);
+
+            // Write the ICS content to the file
+            fileOutputStream.write(icsContent.getBytes());
+            fileOutputStream.close();
+
+            Log.d("ICS", "ICS file saved at: " + icsFile.getAbsolutePath());
+
+            //Debug to make sure the content is being saved
+            //TextView textView = findViewById(R.id.textTitle);
+            //textView.setText(icsContent);
+
+            }
+        catch (IOException e)
+            {
+            Log.e("ICS", "Error saving ICS file: " + e.getMessage());
+            }
+
+        // Display content in TextView
 
     }
 
-    public void cancel(View v) {
+    public void selectDate (View v)
+    {
 
-        Intent i = null;
-
-        i = new Intent(this, RemindersActivity.class);
-
-        startActivity(i);
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
     }
 }
