b9e12ee262905b80356c4c47928748de0ac3d7ad
wallacebd25 <wallacebd25@vmi.edu>
2025-02-25 08:44:43 -0500

    Testing Stage, Back to testing stage all changes made

--- app/src/main/java/com/example/appinterface1/pswdSave.java
+++ app/src/main/java/com/example/appinterface1/pswdSave.java
@@ -1,12 +1,11 @@
 package com.example.appinterface1;
-
+//import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
+//import android.os.Environment;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,9 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import java.io.File; //Added Import
 import java.io.FileOutputStream;
-//import java.io.IOException; //Added Import
+import java.io.IOException; //Added Import
 
 public class pswdSave extends AppCompatActivity {
+
+    //Global Variables
+    String fileName = "PasswordList.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +32,26 @@ public class pswdSave extends AppCompatActivity {
         });
     }
 
+    public void CreateAndWriteFile(String fileName, String Content) {
+        try {
+            //Getting the file path
+            File path = getFilesDir();
+
+            //Create the file
+            File fileP = new File(path, fileName);
+            FileOutputStream fOut = new FileOutputStream(fileP);
+
+            //Write to the file
+            fOut.write(Content.getBytes());
+            fOut.close();
+            Toast.makeText(getApplicationContext(), "Wrote to file: " + fileName, Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     protected void savePSWD() {
-
-        //createFile("Password Storage.txt");
-
         //Needs to be from a text box
         EditText desText = findViewById(R.id.desText);
         String userDec = desText.getText().toString();
@@ -52,41 +70,14 @@ public class pswdSave extends AppCompatActivity {
         String third = ("\nPassword: " + userPass);
         String content = (first + second + third);
 
-        writeToFile("Password Storage.txt", content);
+        //Function that creates and writes to the file
+        CreateAndWriteFile(fileName, content);
     }
 
     public void homeScreen(View H) {
         Intent homeScreen = new Intent(this, MainActivity.class);
         startActivity(homeScreen);
     }
-
-    public void writeToFile(String filename, String content) {
-        File path = getApplicationContext().getFilesDir();
-        try {
-            FileOutputStream writer = new FileOutputStream(new File(path, filename));
-            writer.write(content.getBytes());
-            writer.close();
-            Toast.makeText(getApplicationContext(), "Wrote to file: " + filename, Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-   /* public void createFile(String fileName) {
-        try{
-            File root = new File(Environment.getExternalStorageDirectory(), "My Folder"); //Creating the folder of My Folder
-
-            if(!root.exists()){
-                root.mkdir(); //To make the folder if it does not exist
-            }
-
-            File filepath = new File(root, fileName); //Not sure when the physical file is made directly
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
 
 }
 
